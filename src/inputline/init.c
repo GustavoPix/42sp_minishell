8c555632eb7bbf3c8e8e8e03cab9795ee3cce227
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-03-18 19:23:35 -0300

    norma

--- src/inputline/init.c
+++ src/inputline/init.c
@@ -6,15 +6,16 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 14:29:14 by glima-de          #+#    #+#             */
-/*   Updated: 2022/02/26 16:28:48 by glima-de         ###   ########.fr       */
+/*   Updated: 2022/03/17 20:52:23 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inputline.h"
 
-t_inputline *start_inputline()
+t_inputline	*start_inputline(void)
 {
-	t_inputline *iline;
+	t_inputline	*iline;
+
 	iline = malloc(sizeof(t_inputline));
 	iline->path = calloc(255, sizeof(char));
 	iline->input = calloc(1, sizeof(char));
