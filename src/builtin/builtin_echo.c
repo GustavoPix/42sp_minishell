51c9438ec59699d8c197a0695bc95571cf044b96
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-03-31 21:52:47 -0300

    builtin echo -n

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_echo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wjuneo-f <wjuneo-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 14:31:32 by glima-de          #+#    #+#             */
-/*   Updated: 2022/03/22 21:05:27 by wjuneo-f         ###   ########.fr       */
+/*   Updated: 2022/03/31 21:52:06 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,24 @@
 	// juntar todos os parametros caso existam.
 void	builtin_echo(t_cmd *builtin, int fd[])
 {
-	char *aux;
+	char	*aux;
+	int		i;
 
 	aux = ft_strdup(builtin->parans[1]);
 	if (aux)
 	{
+		i = 1;
 		if (ft_strncmp(aux, "-n", ft_strlen("-n")) == 0)
+			i = 2;
+		while (builtin->parans[i])
 		{
-			write(fd[1], builtin->parans[2], ft_strlen(builtin->parans[2] - 1));
+			ft_putstr_fd(builtin->parans[i], fd[1]);
+			i++;
+			if (builtin->parans[i])
+				ft_putstr_fd(" ", fd[1]);
 		}
-		else
-		{
-			ft_putstr_fd(aux, fd[1]);
-		}
+		if (ft_strncmp(aux, "-n", ft_strlen("-n")) != 0)
+			ft_putchar_fd('\n', fd[1]);
 	}
-	ft_putchar_fd('\n', fd[1]);
 	free(aux);
 }
