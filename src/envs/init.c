0437064dc8d1884a3f475a26f6b364ff867484a1
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-05-15 13:04:39 -0300

    norma

--- src/envs/init.c
+++ src/envs/init.c
@@ -6,7 +6,7 @@
 /*   By: glima-de <glima-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 20:54:35 by glima-de          #+#    #+#             */
-/*   Updated: 2022/05/15 12:41:50 by glima-de         ###   ########.fr       */
+/*   Updated: 2022/05/15 13:03:24 by glima-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,15 @@ void	add_env(t_data *data, char *env_value)
 	int		i;
 
 	i = 0;
-	new_envs = malloc(sizeof(t_env *) * (data->qty_env + 1));
-	while (i < data->qty_env)
-	{
-		new_envs[i] = data->envs[i];
-		i++;
-	}
 	aux = ft_split(env_value, '=');
 	if (aux)
 	{
+		new_envs = malloc(sizeof(t_env *) * (data->qty_env + 1));
+		while (i < data->qty_env)
+		{
+			new_envs[i] = data->envs[i];
+			i++;
+		}
 		new_envs[i] = malloc(sizeof(t_env));
 		new_envs[i]->name = ft_strdup(aux[0]);
 		if (aux[1] && ft_strlen(aux[1]))
@@ -52,41 +52,45 @@ void	add_env(t_data *data, char *env_value)
 		data->qty_env++;
 		clear_matrix(aux);
 	}
-	else
-		free(new_envs);
+}
+
+static t_env	**crate_new_env(t_data *data, int pos)
+{
+	int		i;
+	t_env	**new_envs;
+
+	i = 0;
+	new_envs = malloc(sizeof(t_env *) * (data->qty_env - 1));
+	while (i < data->qty_env - 1)
+	{
+		if (i < pos)
+			new_envs[i] = data->envs[i];
+		else
+			new_envs[i] = data->envs[i + 1];
+		i++;
+	}
+	return (new_envs);
 }
 
 void	remove_env(t_data *data, char *env_unset)
 {
 	t_env	**new_envs;
-	int		i;
 	int		pos;
 
 	pos = 0;
-	i = 0;
-	if (env_unset)
-	{
-		while (pos < data->qty_env && ft_strcmp(env_unset, data->envs[pos]->name))
-			pos++;
-		if (pos < data->qty_env)
-		{
-			new_envs = malloc(sizeof(t_env *) * (data->qty_env - 1));
-			while (i < data->qty_env - 1)
-			{
-				if (i < pos)
-					new_envs[i] = data->envs[i];
-				else
-					new_envs[i] = data->envs[i + 1];
-				i++;
-			}
-				free(data->envs[pos]->name);
-				clear_matrix(data->envs[pos]->values);
-				free(data->envs[pos]);
-			free(data->envs);
-			data->envs = new_envs;
-			data->qty_env--;
-		}
-	}
+	if (!env_unset)
+		return ;
+	while (pos < data->qty_env && ft_strcmp(env_unset, data->envs[pos]->name))
+		pos++;
+	if (pos >= data->qty_env)
+		return ;
+	new_envs = crate_new_env(data, pos);
+	free(data->envs[pos]->name);
+	clear_matrix(data->envs[pos]->values);
+	free(data->envs[pos]);
+	free(data->envs);
+	data->envs = new_envs;
+	data->qty_env--;
 }
 
 void	start_envs(t_data *data, char **env)
